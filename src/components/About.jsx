ce58e435f1306b3d2803df135c6cb6c3538ab8a7
mechaniccoder <yuseunghwan94@gmail.com>
2020-08-14 16:30:46 +0900

    Premake UI components

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const AboutWrapper = styled.section`
+  background: #f9f9fc;
+`;
+
+function About() {
+  return <AboutWrapper>about</AboutWrapper>;
+}
+
+export default About;
---
+++ src/components/Else.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const ElseWrapper = styled.section``;
+
+function Else() {
+  return <ElseWrapper>else</ElseWrapper>;
+}
+
+export default Else;
